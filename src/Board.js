f4a05fd1d7dd9d4d403d429b1c77b9d60c51689e
mac00h <m.makuch21@gmail.com>
2022-01-30 00:18:16 +0100

    Food added randomly on board

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const size = 15;
+const size = 30;
+
+function getRandomInt() {
+    return Math.floor(Math.random() * (Math.floor(29) - Math.ceil(0) + 1)) + Math.ceil(0);
+}
 
 const Board = () => {
     const [board, setBoard] = React.useState(new Array(size).fill(0).map(row => new Array(size).fill(0)))
     const [snakeState, setSnakeState] = React.useState('null')
-    const [snake, setSnake] = React.useState({
+    const [snakeSize, setSnakeSize] = React.useState(0);
+    const [snakeHead, setSnakeHead] = React.useState({
         x: 0,
         y: 0,
     })
+
+    const [food, setFood] = React.useState({
+        x: 0,
+        y: 0,
+    })
+
+    React.useEffect(() => {
+        if (snakeHead.x === size) setSnakeHead({ x: 0, y: snakeHead.y })
+        if (snakeHead.x === -1) setSnakeHead({ x: size - 1, y: snakeHead.y })
+        if (snakeHead.y === size) setSnakeHead({ x: snakeHead.x, y: 0 })
+        if (snakeHead.y === -1) setSnakeHead({ x: snakeHead.x, y: size - 1 })
+        if (snakeHead.y === food.y && snakeHead.x === food.x) {
+            setSnakeSize(snakeSize + 1)            
+        }
+    }, [snakeHead.x, snakeHead.y])
+
+    React.useEffect(() => {
+        setFood({
+            x: getRandomInt(),
+            y: getRandomInt()
+        })
+    }, [snakeSize])
 
     React.useEffect(() => {
         window.addEventListener('keydown', handleArrowPress);
@@ -23,36 +50,36 @@ const Board = () => {
         if (snakeState !== 'null') {
             const interval = setInterval(() => {
                 if (snakeState === 'right') {
-                    setSnake({ x: snake.x + 1, y: snake.y })
+                    setSnakeHead({ x: snakeHead.x + 1, y: snakeHead.y })
                 } else if (snakeState === 'left') {
-                    setSnake({ x: snake.x - 1, y: snake.y })
+                    setSnakeHead({ x: snakeHead.x - 1, y: snakeHead.y })
                 } else if (snakeState === 'up') {
-                    setSnake({ x: snake.x, y: snake.y - 1 })
+                    setSnakeHead({ x: snakeHead.x, y: snakeHead.y - 1 })
                 } else if (snakeState === 'down') {
-                    setSnake({ x: snake.x, y: snake.y + 1 })
+                    setSnakeHead({ x: snakeHead.x, y: snakeHead.y + 1 })
                 }
-            }, 400);
+            }, 50);
             return () => clearInterval(interval);
         }
 
-    }, [snakeState, snake.x, snake.y]);
+    }, [snakeState, snakeHead.x, snakeHead.y]);
 
     const handleArrowPress = (e) => {
         switch (e.keyCode) {
             case 37: //left
-                setSnake({ x: snake.x - 1, y: snake.y });
+                setSnakeHead({ x: snakeHead.x - 1, y: snakeHead.y });
                 setSnakeState('left')
                 break;
             case 38: //up
-                setSnake({ x: snake.x, y: snake.y - 1 });
+                setSnakeHead({ x: snakeHead.x, y: snakeHead.y - 1 });
                 setSnakeState('up')
                 break;
             case 39: //right
-                setSnake({ x: snake.x + 1, y: snake.y });
+                setSnakeHead({ x: snakeHead.x + 1, y: snakeHead.y });
                 setSnakeState('right')
                 break;
             case 40: //down
-                setSnake({ x: snake.x, y: snake.y + 1 });
+                setSnakeHead({ x: snakeHead.x, y: snakeHead.y + 1 });
                 setSnakeState('down')
                 break;
         }
@@ -61,11 +88,12 @@ const Board = () => {
     }
 
     return (
-        <div>
+        <div style={{ border: '1px solid white', padding: '10px' }}>
             {board.map((row, indexR) => (
-                <div key={indexR} style={{ height: '50px' }}>
+                <div key={indexR} style={{ height: '25px' }}>
                     {row.map((cell, indexC) => {
-                        return indexR === snake.y && indexC === snake.x ? <Cell key={indexC} color={'green'} /> : <Cell key={indexC} />
+                        return indexR === snakeHead.y && indexC === snakeHead.x ? <Cell key={indexC} color={'white'} />
+                            : indexR === food.y && indexC === food.x ? <Cell key={indexC} color={'red'} /> : <Cell key={indexC} />
                     })}
                 </div>
             ))}
@@ -76,9 +104,8 @@ const Board = () => {
 export default Board;
 
 const Cell = styled.div`
-width: 50px;
-height: 50px;
-outline: 1px solid white;
+width: 25px;
+height: 25px;
 display: inline-block;
 background: ${props => props.color};
 `;
